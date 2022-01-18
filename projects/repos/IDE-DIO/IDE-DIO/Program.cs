8c3789d773d953f5480d40fdf9aafcf0785c07c6
Rei-creator <joao.gbr.ds@gmail.com>
2022-01-18 18:29:04 -0300

    Primeiro projeto

---
+++ projects/repos/IDE-DIO/IDE-DIO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IDE_DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
